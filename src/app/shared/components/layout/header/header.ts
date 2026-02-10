aeb4e004940e338fcbdf0b07a4814d1be160014d
= <=>
2026-02-10 11:13:04 -0500

    ajustando diseño nav

--- src/app/shared/components/layout/header/header.ts
+++ src/app/shared/components/layout/header/header.ts
@@ -1,21 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
 })
 export class HeaderComponent {
-  menuOpen = false;
+  @Input() userName: string = 'Juan Pérez';
+  @Input() userRole: string = 'Administrador';
 
-  toggleMenu(): void {
-    this.menuOpen = !this.menuOpen;
+
+  @Output() addClicked = new EventEmitter<void>();
+
+  constructor(private router: Router) {}
+
+  // 👤 Iniciales del usuario
+  get initials(): string {
+    if (!this.userName) return '';
+    return this.userName
+      .split(' ')
+      .map(name => name[0])
+      .slice(0, 2)
+      .join('')
+      .toUpperCase();
   }
 
-  closeMenu(): void {
-    this.menuOpen = false;
+  // Acción del botón (Nuevo Reporte)
+  onAdd(): void {
+    // this.router.navigate(['/report/new']);
   }
+
+
 }
