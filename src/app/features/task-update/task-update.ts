92e3a5ea3feb54e9cf0eb9c5c9beca0a0bea1131
Daniel Mercado <dms9812@hotmail.com>
2026-02-26 06:34:11 -0500

    Routes for tasnk deatail, task list and task update

---
+++ src/app/features/task-update/task-update.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-update',
+  imports: [],
+  templateUrl: './task-update.html',
+  styleUrl: './task-update.scss',
+})
+export class TaskUpdate {
+
+}
--- src/app/layout/routes/private.routes.ts
+++ src/app/layout/routes/private.routes.ts
@@ -1,14 +1,30 @@
 import { Routes } from '@angular/router';
+import { Role } from '@enums/role';
 
 export const privateRoutes: Routes = [
   {
     path: '',
     loadComponent: () => import('../../features/home/home').then( component => component.Home ),
-    data: { roles: ["ADMIN", "REPORTER"] }
+    data: { roles: [Role.ADMIN, Role.REPORTER, Role.MAINTENANCE] }
   },
   {
     path: 'create-report',
     loadComponent: () => import('../../features/create-report/create-report').then( component => component.CreateReport),
-    data: { roles: ["REPORTER", "ADMIN"] }
-  }
+    data: { roles: [Role.ADMIN, Role.REPORTER, Role.MAINTENANCE] }
+  },
+  {
+    path: 'task-list',
+    loadComponent: () => import('../../features/task-list/task-list').then( component => component.TaskList ),
+    data: { roles: [Role.ADMIN, Role.REPORTER, Role.MAINTENANCE] }
+  },
+  {
+    path: 'task/:taskId/update',
+    loadComponent: () => import('../../features/task-update/task-update').then( component => component.TaskUpdate ),
+    data: { roles: [Role.ADMIN, Role.REPORTER, Role.MAINTENANCE] }
+  },
+  {
+    path: 'task/:taskId',
+    loadComponent: () => import('../../features/task-detail/task-detail').then( component => component.TaskDetail ),
+    data: { roles: [Role.ADMIN, Role.REPORTER, Role.MAINTENANCE] }
+  },
 ];
